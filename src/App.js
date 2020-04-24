1e6da5c9f34f1aabe26a9c484d5b5f73f59d632a
matthew-heideman <heidemanmr@yahoo.com>
2020-04-24 11:20:49 -0500

    wip

--- src/App.js
+++ src/App.js
@@ -1,10 +1,23 @@
 import React from "react";
+import HomePage from './Components/HomePage';
+import { Switch, Route } from 'react-router-dom';
+import { Nav, NavItem } from 'reactstrap';
 
 const App = () => {
   return (
     <>
-      <h1>Lambda Eats</h1>
-      <p>You can remove this code and create your own header</p>
+      <Nav>
+        <NavItem>
+        
+        </NavItem>
+        <NavItem>
+
+        </NavItem>
+      </Nav>
+      <Switch>
+        <Route path = '/' component={HomePage}/>
+      </Switch>
+      
     </>
   );
 };
